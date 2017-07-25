6c06fd37906c45e4d3ef12acd91dd402a1aec206
Dylan Nguyen <Dnguye92@yahoo.com>
2017-07-25 16:54:19 -0700

    parsed inputs to HTML elements. Need to add more options for adding more li's

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
-// var newLinks = document.getElementById('new');
-// console.log(newLinks);
+var	button = document.getElementById('button'),
+	results = document.getElementById('results');
 
-// var subCat = document.getElementsByClassName('subCat');
-// console.log(subCat);
+button.addEventListener('click', function() {
+	// console.log(input.value + ' ' + url.value)
+	var ul = document.getElementById('categories');
+	var li = document.createElement('li');
+	var a = document.createElement('a');
+	console.log(ul);
 
-// newLinks.addEventListener('click', function() {
-// 	subCat.style.display = 'show';
-// })
+	for(var i = 0; i < 5; i++) {
+		var input = document.getElementById('input' + [i]);
+		var link = document.getElementById('urlValue' + [i]);
+		var br = document.createElement('br');
+		var h1 = document.createElement('h1');
+		var parsed;
+		parsed = '<li>' + '<a href="' + link.value + '>' + input.value + '</a>' + '</li>';
+		h1.textContent = parsed;
+
+		results.appendChild(h1);
+	
+	}
+	// results.textContent = '<a href="' + url.value + '>' + input.value + '</a>';
+})
