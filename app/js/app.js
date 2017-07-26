33dbde48ccb5805a3ba228aeeaa2f43a64e1ad40
Dylan Nguyen <Dnguye92@yahoo.com>
2017-07-26 07:07:19 -0700

    update

--- app/js/app.js
+++ app/js/app.js
@@ -10,49 +10,49 @@ CrNav.config(['$routeProvider', function($routeProvider) {
 CrNav.controller('NewLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/newLinks.json').success(function(data) {
 		$scope.newLinkData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('ShoesLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/shoesLinks.json').success(function(data) {
 		$scope.shoesLinkData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('DressLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/dressLinks.json').success(function(data) {
 		$scope.dressLinkData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('ClothesLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/clothesLinks.json').success(function(data) {
 		$scope.clothesLinkData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('PlusLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/plusLinks.json').success(function(data) {
 		$scope.plusLinksData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('AccLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/accLinks.json').success(function(data) {
 		$scope.accLinksData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
 CrNav.controller('SaleLinksCtrl', ['$scope', '$http', function($scope, $http) {
 	$http.get('./js/navLinks/saleLinks.json').success(function(data) {
 		$scope.saleLinksData = data;
-		console.log(data);
+		// console.log(data);
 	})
 }])
 
--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,78 @@
-var	button = document.getElementById('button'),
-	results = document.getElementById('results');
+// var	button = document.getElementById('button'),
+// 	results = document.getElementById('results');
 
-button.addEventListener('click', function() {
-	// console.log(input.value + ' ' + url.value)
-	var ul = document.getElementById('categories');
-	var li = document.createElement('li');
-	var a = document.createElement('a');
-	console.log(ul);
+// button.addEventListener('click', function() {
+// 	// console.log(input.value + ' ' + url.value)
+// 	var li = document.createElement('li');
+// 	var a = document.createElement('a');
+// 	console.log(ul);
 
-	for(var i = 0; i < 5; i++) {
-		var input = document.getElementById('input' + [i]);
-		var link = document.getElementById('urlValue' + [i]);
-		var br = document.createElement('br');
-		var h1 = document.createElement('h1');
-		var parsed;
-		parsed = '<li>' + '<a href="' + link.value + '>' + input.value + '</a>' + '</li>';
-		h1.textContent = parsed;
+// 	// for(var i = 0; i < 5; i++) {
+// 	// 	var input = document.getElementById('input' + [i]);
+// 	// 	var link = document.getElementById('urlValue' + [i]);
+// 	// 	var br = document.createElement('br');
+// 	// 	var h1 = document.createElement('h1');
+// 	// 	var parsed;
+// 	// 	parsed = '<li>' + '<a href="' + link.value + '>' + input.value + '</a>' + '</li>';
+// 	// 	h1.textContent = parsed;
 
-		results.appendChild(h1);
+// 	// 	results.appendChild(h1);
 	
+// 	// }
+// 	// results.textContent = '<a href="' + url.value + '>' + input.value + '</a>';
+// })
+
+var results = document.getElementById('results');
+var listAmount = document.getElementById('listAmount');
+var generateBtn = document.getElementById('generateBtn');
+
+generateBtn.addEventListener('click', function() {
+	var totalListAmount = parseInt(listAmount.value);
+	var parseBtn = document.createElement('button');
+	var subCatNameArr = [];
+	var subCatURLArr = [];
+
+	parseBtn.className = 'btn btn-primary';
+	parseBtn.textContent = 'Parse HTML';
+
+	console.log(totalListAmount);
+
+	for(var i = 0; i < totalListAmount; i++) {
+		var subCatName = document.createElement('input');
+		var subCatURL = document.createElement('input');
+		var catNum = document.createElement('h2');
+
+		catNum.textContent = i + 1;
+
+
+		subCatName.className = 'subCatName form-control';
+		subCatName.setAttribute('placeholder', 'Enter Category Name');
+
+		subCatURL.className = 'subCatURL form-control';
+		subCatURL.setAttribute('placeholder', 'Enter URL');
+
+		results.appendChild(catNum);
+		results.appendChild(subCatName);
+		results.appendChild(subCatURL);
+
+		console.log(subCatName);
+		console.log(subCatURL);
+
 	}
-	// results.textContent = '<a href="' + url.value + '>' + input.value + '</a>';
+
+	results.appendChild(parseBtn);
+
+	parseBtn.addEventListener('click', function() {
+		console.log('This second function works!');
+		console.log(document.getElementsByClassName('subCatName'))
+		for(var i = 0; i < totalListAmount; i++) {
+			
+		}
+
+	})
 })
+
+
+
+
+
